ae6e1fa6a43b39d56e53d3b681e7522657574db3
Tarpan Pathak <tarpan.x.pathak.-nd@disney.com>
2019-02-03 17:19:08 -0800

    First commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port := ""
+	if len(os.Args) == 2 {
+		port = ":" + os.Args[1]
+	} else {
+		port = ":8080"
+	}
+
+	fmt.Println("Server started at http://localhost:" + port[1:])
+	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(pwd))))
+}
